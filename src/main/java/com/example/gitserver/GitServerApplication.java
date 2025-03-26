6599814df72e936a4d01305fd37c0d6f5a4defc4
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-03-26 17:45:13 +0900

    🎉 프로젝트 초기세팅

---
+++ src/main/java/com/example/gitserver/GitServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitServerApplication.class, args);
+    }
+
+}
