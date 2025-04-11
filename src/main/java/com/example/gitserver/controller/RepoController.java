6c1b83fed25583b72ecf62ed9797268511e8d6a8
kimjungminn24 <kimjungminn24@users.noreply.github.com>
2025-04-11 17:56:48 +0900

    ✨ 브랜치 머지 기능 추가

--- src/main/java/com/example/gitserver/controller/RepoController.java
+++ src/main/java/com/example/gitserver/controller/RepoController.java
@@ -1,15 +1,14 @@
 package com.example.gitserver.controller;
 
+import com.example.gitserver.dto.MergeDto;
 import com.example.gitserver.dto.RepoInfoDto;
 import com.example.gitserver.service.RepoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/repo")
@@ -19,8 +18,16 @@ public class RepoController {
     private final RepoService repoService;
 
     @PostMapping
-    public ResponseEntity<Void> createRepo(@Valid @RequestBody RepoInfoDto repoInfoDto){
+    public ResponseEntity<Void> createRepo(@Valid @RequestBody RepoInfoDto repoInfoDto) {
         repoService.createRepo(repoInfoDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+
+    @PostMapping("/{owner}/{repo}")
+    public ResponseEntity<Void> mergeBranch(@PathVariable String owner,@PathVariable String repo, @RequestBody MergeDto mergeDto) {
+        repoService.mergeBranch(owner,repo,mergeDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
